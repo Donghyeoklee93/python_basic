c59f265a4af3d4a15207e9972a255a8acda78864
Donghyeok Lee <donghyukleee@gmail.com>
2023-08-10 15:13:24 +0100

    data type, variable

---
+++ myvenv/chapter3/01.data_type.py
@@ -0,0 +1,29 @@
+# Comments
+# 1. When you want to add comments for code
+# 2. When you don't want to run code.
+
+
+# Number data type
+# 1. integer
+print(1)
+print(3)
+print(0)
+print(-1)
+print(1, 3, 0, -1, sep="")
+
+
+# 2. float
+print(1.1, 3.1, 0, -1.4)
+
+
+# string
+# "" or ''
+print("python", end='')
+print('python')
+print('"python is funny!"')
+
+
+# booleanaa
+# True or False
+print(True)
+print(False)
---
+++ myvenv/chapter3/02.variable.py
@@ -0,0 +1,15 @@
+# variable
+# variable name = data
+
+name = "mater"
+level = 5
+health = 800
+attack = 90
+print(name, level, health, attack)
+
+
+#change data
+level = level + 1
+health = health + 50
+attack = attack + 10
+print(name, level, health, attack)
